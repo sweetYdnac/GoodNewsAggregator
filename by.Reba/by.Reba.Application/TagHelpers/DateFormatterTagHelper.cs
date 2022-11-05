433ffbe48215969f9c0926e255af8696296bb28e
sweetYdnac <96253105+sweetYdnac@users.noreply.github.com>
2022-11-05 18:25:16 +0300

    add bootstrap and css to article index page

--- by.Reba/by.Reba.Application/TagHelpers/DateFormatterTagHelper.cs
+++ by.Reba/by.Reba.Application/TagHelpers/DateFormatterTagHelper.cs
@@ -1,26 +1,27 @@
 ﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Globalization;
 
 namespace by.Reba.Application.TagHelpers
 {
     // todo : fix this
     public class DateFormatterTagHelper : TagHelper
     {
+        [HtmlAttributeName("asp-for")]
         public DateTime Date { get; set; }
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            output.TagMode = TagMode.SelfClosing;
-            output.TagName = "p";
+            output.TagName = "span";
 
             var time = DateTime.Now - Date;
             var formated = time.Days switch
             {
-                >= 2 => Date.ToString("f"),
-                1 => $"Вчера в {Date.ToString("t")}",
-                0 => $"Сегодня в {Date.ToString("t")}",
+                >= 2 => Date.ToString("dd MMMM, HH:mm", new CultureInfo("ru-RU")),
+                1 => $"Вчера в {Date.ToString("HH:mm")}",
+                0 => $"Сегодня в {Date.ToString("hh:mm")}",
                 _ => time.Hours < 10 ? $"{time.Hours} час. назад" : $"{time.Minutes} мин. назад",
             };
 
-            output.Content.SetContent(formated);
+            output.Content.AppendHtml(formated);
         }
     }
 }

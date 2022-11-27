0d19cb41e6a8f86c10f60d729d2017b118dcc930
sweetYdnac <96253105+sweetYdnac@users.noreply.github.com>
2022-11-27 23:48:37 +0300

    Remove identity template project

--- by.Reba/IdentityTemplate/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace IdentityTemplate.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
